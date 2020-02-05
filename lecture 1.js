fa7088f7da0c1a87a895d79caf8e1cc655f23efe
Frederik <34657773+fljzenziot@users.noreply.github.com>
2020-02-05 17:57:06 +0100

    Update lecture 1.js

--- lecture 1.js
+++ lecture 1.js
@@ -4,8 +4,7 @@ let s = "";
 let sArray = ["12", "1234", "12", "1234", "12", "1234"];
 
 //task 2.
-let createChessTable = function test() 
-{
+let createChessTable = function test() {
     for (let i = 0; i < height; i++) {
         for (let j = 0; j < width; j++) {
             if (i % 2 === 0)
@@ -28,8 +27,7 @@ function AddTwoVariablesTogether(oldVal, newVal) {
     return oldVal + newVal;
 }
 
-let AverageStringLength = function CalcuLateAvgStrLgn(strArray) 
-{
+let AverageStringLength = function CalcuLateAvgStrLgn(strArray) {
     let totalLgn = 0;
 
     if (Array.isArray(strArray)) {
@@ -37,46 +35,83 @@ let AverageStringLength = function CalcuLateAvgStrLgn(strArray)
         //console.log("Total:" + totalLgn);
         return totalLgn / strArray.length;
     }
-    else
-    {
+    else {
         return -1;
     }
 }
 console.log("Average string length: " + AverageStringLength(sArray));
 
 //Task 4. not finish
-function findSolution(target) { 
+function findSolution(target) {
     function find(current, history) {
-      if (current == target) {
-        return history;
-      } else if (current > target) {
-        return null;
-      } else {
-        return find(current + 5, `(${history} + 5)`) ||
-               find(current * 3, `(${history} * 3)`);
-      }
+        if (current == target) {
+            return history;
+        } else if (current > target) {
+            return null;
+        } else {
+            return find(current + 5, `(${history} + 5)`) ||
+                find(current * 3, `(${history} * 3)`);
+        }
     }
     return find(1, "1");
-  }
+}
 
-  console.log(findSolution(18));
+console.log(findSolution(18));
 
 
-  //Task 5.
-  let journal = [
-    {events: ["work", "touched tree", "pizza",
-              "running", "television"],
-     squirrel: false},
-    {events: ["work", "ice cream", "cauliflower",
-              "lasagna", "touched tree", "brushed teeth"],
-     squirrel: false},
-    {events: ["weekend", "cycling", "break", "peanuts",
-              "beer"],
-     squirrel: true},
+//Task 5.
+let journal = [
+    {
+        events: ["work", "touched tree", "pizza",
+            "running", "television"],
+        squirrel: false
+    },
+    {
+        events: ["work", "ice cream", "cauliflower",
+            "lasagna", "touched tree", "brushed teeth"],
+        squirrel: false
+    },
+    {
+        events: ["weekend", "cycling", "break", "peanuts",
+            "beer"],
+        squirrel: true
+    },
     /* and so on... */
-  ];
+];
 
-  console.log(journal[2].events[4]);
-  
+console.log(journal[2].events[4]);
+
+
+//Task 6.
+strInfo = { min: 0, max: 0, average: 0, median: 0 } //result obj
+
+function StringArrayInfo(strArray) {
+    if (Array.isArray(strArray)) {
+        let s = strInfo
+        strArray.sort(function(a, b){return a.length - b.length});
+        s.min = strArray[0].length;
+        s.max = strArray[strArray.length - 1].length
+        s.average = AverageStringLength(strArray);
+
+        if(strArray.length % 2 == 0)
+        {
+            let med = strArray.length / 2;
+            let i = strArray[med].length;
+            i += strArray[med -1].length;
+            s.median = i / 2;
+        }
+        else
+        {
+            console.log("here2");
+            s.median = strArray[strArray.length / 2 - 1].length;
+        }
+        console.log(strArray);
+
+        return s;
+    }
+    return null;
+}
+
+console.log(StringArrayInfo(sArray));
 
 
